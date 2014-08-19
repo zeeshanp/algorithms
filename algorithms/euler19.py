a6f91266157054ddb614469b6c2e7f909ed5c91d
Zeeshan Pirzada <pirzadaza@gmail.com>
2014-08-18 18:57:41 -0700

    Fixed #19 works now

--- algorithms/euler19.py
+++ algorithms/euler19.py
@@ -9,12 +9,17 @@ months = [31,28,31,30,31,30,31,31, 30,31,30,31]
 if __name__ == '__main__':
     num = 0
     date = 1
-    day_of_week = 0
+    day_of_week = 1
     leap = False
-    #Jan 1st, 1900 was a monday.
-    for year in range(1900, 2001):
-        leap = (year % 4 == 0)
-     
+    #Jan 1st, 1901 was a tuesday.
+    for year in range(1901, 2001):
+        if (year%100 == 0 and year%400 == 0):
+            leap = True
+        elif (year%100 == 0 and year%400 != 0):
+            leap = False
+        else:
+            leap = (year % 4 == 0)
+
         for m in range(0,12):
             if leap == True and m == 1:
                 curr = 29
@@ -25,6 +30,6 @@ if __name__ == '__main__':
                 day_of_week = (day_of_week + 1)%7
                 if day_of_week == 6 and date == 1:
                     num = num + 1
-            date = 1
+            date = 0
 
     print num

a9dd0490ef93ff23a3071c6df7d07e8a88d4bac0
Zeeshan <pirzadaza@gmail.com>
2015-04-20 03:29:16 -0700

    remove node

--- algorithms/bst.cpp
+++ algorithms/bst.cpp
@@ -16,6 +16,7 @@ public:
 	Bst();
 
 	bool insert(string& a);
+	bool remove(string& a);
 	void printInOrder();
 
 	~Bst();
@@ -24,10 +25,13 @@ private:
 
 	struct node
 	{
-		node(string& val);
+		// if left is true, node is a left child.
+		node(string& _val, node *_parent, bool _leftchild);
 		string val;
 		node *left;
 		node *right;
+		node *parent;
+		bool leftchild;
 	};
 	
 	void inOrderTraversal(node *temp);
@@ -35,11 +39,13 @@ private:
 
 };
 
-Bst::node::node(string& _val)
+Bst::node::node(string& _val, node *_parent, bool _leftchild)
 {
 	val = _val;
 	left = NULL;
 	right = NULL;
+	parent = _parent;
+	leftchild = _leftchild;
 }
 
 
@@ -57,7 +63,7 @@ bool Bst::insert(string& a)
 {
 	if (m_head == NULL)
 	{
-		m_head = new node(a);
+		m_head = new node(a, NULL, false);
 		return true;
 	}
 	
@@ -66,24 +72,22 @@ bool Bst::insert(string& a)
 	while (true)
 	{
 		if (a < temp->val)
-		{
-			
+		{	
 			if (temp->left)
 				temp = temp->left;
 			else
 			{
-				temp->left = new node(a);
+				temp->left = new node(a, temp, true);
 				return true;
 			}
 		}
 		else
-		{ 
-			
+		{ 	
 			if (temp->right)
 				temp = temp->right;
 			else
 			{
-				temp->right = new node(a);
+				temp->right = new node(a, temp, false);
 				return true;
 			}
 		}
@@ -91,6 +95,109 @@ bool Bst::insert(string& a)
 
 	
 	return true;
+}
+
+bool Bst::remove(string& a)
+{
+	
+	node *temp = m_head;
+
+	while (temp) 
+	{
+		// found node to delete
+		if (temp->val == a)
+		{
+			// no children
+			if (temp->left == NULL && temp->right == NULL)
+			{
+				if (temp->parent)
+				{
+					if (temp->leftchild)
+						temp->parent->left = NULL;
+					else
+						temp->parent->right = NULL;
+
+					delete temp;
+				}
+				else
+					delete m_head;
+
+				return true;
+			}
+
+			// one child on right
+			if (temp->left == NULL && temp->right != NULL)
+			{
+				if (temp->parent)
+				{
+					if (temp->leftchild)
+						temp->parent->left = temp->right;
+					else
+						temp->parent->right = temp->right;
+
+					delete temp;
+				}
+				else
+				{
+					m_head = temp->right;
+					delete temp; 
+				}
+				return true;
+
+			}
+
+			// one child on left
+			if (temp->left != NULL && temp->right == NULL)
+			{
+				if (temp->parent)
+				{
+					if (temp->leftchild)
+						temp->parent->left = temp->left;
+					else
+						temp->parent->right = temp->left;
+
+					delete temp
+				}
+				else
+				{
+					m_head = temp->left;
+					delete temp;
+				}
+
+				return true;
+			}
+
+			// two children
+			if (temp->left != NULL && temp->right != NULL)
+			{
+				// replace temp with largest val in left subtree
+				// then delete said value
+				node *temp2 = temp->left;
+				
+				while (temp2)
+					temp2 = temp2->right;
+
+				temp->val = temp2->val;
+				
+				if (temp2->leftchild)
+					temp2->parent->left = NULL;
+				else
+					temp2->parent->right = NULL;
+
+				delete temp2;
+
+			}
+
+		}
+		else if (a < temp->val)
+			temp = temp->left;
+		else
+			temp = temp->right;
+
+	}
+
+
+	return false;
 }
 
 void Bst::printInOrder()
@@ -112,10 +219,10 @@ void Bst::inOrderTraversal(node *temp)
 int main()
 {
 	Bst tree;
-	cout << "Enter in words and they will stay alphabetized!!" << endl;
+	cout << "Enter in words and they will stay alphabetized, enter word again to remove." << endl;
 	for (;;)
 	{
-		cout << "Enter a word:";
+		cout << "Enter a word: ";
 		string a;
 		cin >> a;
 		tree.insert(a);

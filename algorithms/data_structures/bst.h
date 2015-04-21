c3f9e56bc8cb618a08664cbf3816fa96a8393510
Zeeshan <pirzadaza@gmail.com>
2015-04-20 17:18:28 -0700

    organized

---
+++ algorithms/data_structures/bst.h
@@ -0,0 +1,43 @@
+#ifndef __BST__
+#define __BST__
+
+#include <iostream>
+#include <string>
+
+
+// binary search tree of strings, for auto
+// alphabetization
+
+class Bst
+{
+
+public:
+	
+	Bst();
+
+	bool insert(std::string& a);
+	bool remove(std::string& a);
+	bool contains(std::string& a);
+	void printInOrder();
+
+	~Bst();
+
+private:
+
+	struct node
+	{
+		// if left is true, node is a left child.
+		node(std::string& _val, node *_parent, bool _leftchild);
+		std::string val;
+		node *left;
+		node *right;
+		node *parent;
+		bool leftchild;
+	};
+	
+	void inOrderTraversal(node *temp);
+	node *m_head;
+
+};
+
+#endif
